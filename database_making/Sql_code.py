94ee8e6af0ff662e5f52f8bffb565ed3a520b95e
Abhay Singh Chauhan <92663561+INEcodes@users.noreply.github.com>
2024-10-17 21:44:30 +0530

    Attendance_1

---
+++ database_making/Sql_code.py
@@ -0,0 +1,8 @@
+# will form the attendance based databse for the employees entry and exit, kind off a system.
+
+from faker import Faker 
+fake = Faker()  
+print(fake.name()) 
+
+
+#making the database from the data, for retrieving the values of the database for the attendance.
